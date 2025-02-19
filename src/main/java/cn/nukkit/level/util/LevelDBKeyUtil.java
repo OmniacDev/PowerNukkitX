d66e483cf239cb5de874cc42b16e24a66fffc9d7
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2025-02-20 02:29:11 +1300

    push

--- src/main/java/cn/nukkit/level/util/LevelDBKeyUtil.java
+++ src/main/java/cn/nukkit/level/util/LevelDBKeyUtil.java
@@ -90,6 +90,7 @@ public enum LevelDBKeyUtil {
         bb.put(NEW_ENTITIES_INDEX_KEY);
         bb.putLong(entityId);
         return bb.array();
+
     }
 
     public static byte[] getChunkKey(int chunkX, int chunkZ) {
